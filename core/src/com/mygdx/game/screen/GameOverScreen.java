2c24b3178126b949b6d3d70bc0cda791ca07a62a
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2022-08-17 06:24:08 +0600

    Color and other things fixed by Hafiz

--- core/src/com/mygdx/game/screen/GameOverScreen.java
+++ core/src/com/mygdx/game/screen/GameOverScreen.java
@@ -51,7 +51,7 @@ public class GameOverScreen implements Screen {
 
     @Override
     public void render (float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(234, 220, 56, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
 
